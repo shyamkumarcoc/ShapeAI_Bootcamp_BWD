0cf174d5c952cd3235f78f03ff8310b5c1bfc6a7
shyamkumarcoc <85401007+shyamkumarcoc@users.noreply.github.com>
2021-06-05 14:49:09 +0100

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,9 @@
+import  React from "react";
+function Footer() {
+var currYear = new Date().getFullYear();
+return (
+<footer> <p>Copyright @ {currYear}</p>
+</footer> 
+);
+}
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Note() {
+return (
+<div className="note"> <h1>Javascript and React.js</h1>
+<p> This was an amazing bootcamp taken by Shaurya Sinha ,We covered everything
+from Scratch including JAvascript React.js, HTML.</p>
+</div>
+);
+}
+export default Note;
